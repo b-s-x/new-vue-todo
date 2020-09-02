53ea958a20af146e83ff52d12d2c25b563ce1989
b-s-x <b.s.x.mars@gmail.com>
2020-09-02 11:23:32 +0300

    relocated methods in external

--- src/views/List.vue
+++ src/views/List.vue
@@ -7,19 +7,19 @@
             </ListModalFrame>
         </list-transition>
 
-        <ListAddItem class='add'/>
+        <ListAddItem class="add"/>
 
         <select v-model="filter" class="selectForm">
-            <option value='all'> All </option>
-            <option value='completed'> Completed </option>
-            <option value='not-completed'> Not Completed </option>
+            <option value="all"> All </option>
+            <option value="completed"> Completed </option>
+            <option value="not-completed"> Not Completed </option>
         </select>
 
-        <ListLoader class='loader' v-if="getLoading"/>
+        <ListLoader class="loader" v-if="getLoading"/>
 
         <ListRender class="render"
             v-else-if="filterItem.length"
-            :todos='filterItem'
+            :todos="filterItem"
         />
 
         <p v-else class="nothing"> Nothing... </p>  
@@ -28,20 +28,34 @@
 </template>
 
 <script>
-import ListRender from '@/components/render/ListRender'
-import ListAddItem from '@/components/render/ListAddItem'
-import ListLoader from '@/components/ui/ListLoader'
-import ListModalFrame from '@/components/ui/ListModalFrame'
-import ListTransition from '@/components/ui/ListTransition'
+import ListRender from "@/components/render/ListRender"
+import ListAddItem from "@/components/render/ListAddItem"
+import ListLoader from "@/components/ui/ListLoader"
+import ListModalFrame from "@/components/ui/ListModalFrame"
+import ListTransition from "@/components/ui/ListTransition"
 
-import { bus } from '../main.js'
+import { bus } from "../main.js"
+
+
+const removeItem = function(id) {
+    this.$store.commit("remove", id)
+};
+        
+const addItem = function(dataPart) {
+    this.$store.commit("add", dataPart)
+};
+
+const closeModal = function() {
+    this.isVisible = false;
+};
+
 
 export default {
     data() {
         return {
-            filter: 'all',
+            filter: "all",
             isVisible: false,
-            id: '',
+            id: "",
         }
     },
 
@@ -54,29 +68,20 @@ export default {
     },
 
     methods: {
-        removeItem(id) {
-            this.$store.commit('remove', id)
-        },
-        
-        addItem(dataPart) {
-            this.$store.commit('add', dataPart)
-        }, 
-
-        closeModal() {
-            this.isVisible = false;
-        },
-
+        removeItem,
+        addItem,
+        closeModal
     },
 
     computed: {
         filterItem() {
-            if(this.filter == 'all') {
+            if(this.filter == "all") {
                 return this.allTodos
             }   
-            if(this.filter == 'completed') {
+            if(this.filter == "completed") {
                 return this.allTodos.filter((elem) => elem.completed)
             }
-            if(this.filter == 'not-completed') {
+            if(this.filter == "not-completed") {
                 return this.allTodos.filter((elem) => !elem.completed)
             }
         },
@@ -91,40 +96,41 @@ export default {
     },
 
     mounted() {
-        this.$store.dispatch('fetchTodos');
+        this.$store.dispatch("fetchTodos");
     },
 
     created() {
-        bus.$on('addItem', (dataPart) => {
+        bus.$on("addItem", (dataPart) => {
             this.addItem(dataPart)
         })
 
-        bus.$on('accept', () => {
+        bus.$on("accept", () => {
             this.removeItem(this.id);
             this.closeModal()
         })
 
-        bus.$on('decline', () => {
+        bus.$on("decline", () => {
             this.closeModal()
         })
         
-        bus.$on('isVisible', (id) => {
+        bus.$on("isVisible", (id) => {
             this.id = id
             this.isVisible = true;
         })
     },
     
     beforeDestroy() {
-        bus.$off('addItem');
-        bus.$off('accept');
-        bus.$off('decline');
-        bus.$off('isVisible');
+        bus.$off("addItem");
+        bus.$off("accept");
+        bus.$off("decline");
+        bus.$off("isVisible");
     },
 }
+
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 
-    @import '@/styles/style/list.style.scss';
+    @import "@/styles/style/list.style.scss";
     
 </style>
